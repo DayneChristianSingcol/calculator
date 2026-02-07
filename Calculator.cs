612671ea2476965ec27f87e47a0c4c95aeb64b1b
DayneChristianSingcol <daynesingcol1213@gmail.com>
2026-02-07 10:35:49 +0800

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -2,11 +2,56 @@ using System;
 
 public static class Calculator
 {
-    public static double? Calculate(int first, int second, string operation)
+    public static double? Calculate(int num1, int num2, string operation)
     {
-        //write your business logic in here
-
-        return null;
+        // calculate method 
+       switch(operation){
+              //add
+              case "+":
+                    return = num1 + num2; 
+                break;
+                
+              //subtract
+              case "-":
+                    return = num1 - num2;
+                break;
+                
+              //multiply
+              case "*":
+                    return = num1 * num2;
+                break;
+                
+              //divide
+              case "/":
+                if(num2 == 0){  
+                    Console.WriteLine("Cannot divide by zero");
+                    return null;
+                }
+                
+                else{
+                    return(double) num1 / num2;
+                }
+                break;
+                
+              //modulo
+                case "%":
+                if(num2 == 0){  
+                    Console.WriteLine("Cannot modulo by zero");
+                    return null;
+                }
+                else{
+                    return = num1 % num2;
+                }
+                break;
+                
+              //exit     
+              case "=":
+                    Environment.Exit(0);
+                    return 0;
+                break;
+            default:
+                Console.WriteLine("Invalid Operarion Used, Please Try againn");
+                return null;
     }
 }
 
